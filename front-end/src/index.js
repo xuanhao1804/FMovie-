195b359e44303afe1bf7f99334cef83dcc18f333
Manh <manuhy04@gmail.com>
2024-10-04 08:36:45 +0700

    [DEV] Implement Redux

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-
+import { store } from './store';
+import { Provider } from "react-redux"
 // import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 // import "../node_modules/bootstrap/dist/js/bootstrap.bundle.js";
 // import "../node_modules/bootstrap-icons/font/bootstrap-icons.css"
@@ -10,7 +11,9 @@ import './styles/index.scss'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
---
+++ front-end/src/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit"
+import UserReducer from "./reducers/UserReducer"
+
+export const store = configureStore({
+    reducer: {
+        user: UserReducer
+    }
+})
