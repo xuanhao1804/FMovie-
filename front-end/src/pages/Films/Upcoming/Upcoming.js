7b17c090447454a9323a704ab61f6d319bd75c0c
Manh <manuhy04@gmail.com>
2024-10-09 09:52:39 +0700

    dev

---
+++ front-end/src/pages/Films/Upcoming/Upcoming.js
@@ -0,0 +1,10 @@
+
+const Upcoming = () => {
+    return (
+        <div className=" content-width-padding content-height-padding">
+            coming
+        </div>
+    )
+}
+
+export default Upcoming
