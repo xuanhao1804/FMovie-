9322626f030dde1fa6b1a9b4768b52e1ac96090e
^Zin^ <144225954+Hminh11@users.noreply.github.com>
2024-10-13 22:03:20 +0700

    data-home (#6)

--- front-end/src/components/FilmsCard/FilmsCard.js
+++ front-end/src/components/FilmsCard/FilmsCard.js
@@ -1,20 +1,41 @@
 import "./FilmsCard.scss"
+import { Button, Modal } from 'antd';
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
-const FilmsCard = ({ image, limit, star }) => {
+const FilmsCard = ({ image, limit, star, video }) => {
+    const [open, setOpen] = useState(false);
     return (
-        <div className="films-card" style={{ backgroundImage: `url(${image})` }}>
-            <span className="films-card-age-limitation">
-                {limit}
-            </span>
-            <span className="films-card-rating">
-                {star} <i style={{ color: "yellow" }} className="fa-solid fa-star"></i>
-            </span>
-            <div className="films-card-button">
-                <button className="films-card-button-detail">XEM CHI TIẾT</button>
-                <button className="films-card-button-booking">ĐẶT VÉ</button>
+        <>
+            <div className="films-card" style={{ backgroundImage: `url(${image})` }}
+                onClick={() => setOpen(true)}>
+
+                <span className="films-card-age-limitation">
+                    {limit}
+                </span>
+                <span className="films-card-rating">
+                    {star} <i style={{ color: "yellow" }} className="fa-solid fa-star"></i>
+                </span>
+                <div className="films-card-button">
+                    <button  className="films-card-button-detail">XEM CHI TIẾT</button>
+                    <button className="films-card-button-booking">ĐẶT VÉ</button>
+                </div>
             </div>
-        </div>
+            <Modal
+
+                centered
+                open={open}
+
+                onCancel={() => setOpen(false)}
+                width={1054}
+            >
+                
+                <iframe width="1004" height="565" src={video} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+               
+            </Modal>
+        </>
     )
+
 }
 
 export default FilmsCard
