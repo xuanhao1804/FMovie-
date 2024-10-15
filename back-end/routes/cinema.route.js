55dfc26e29ffb418f5c0b75d6f8d4879c45e5cc4
xuanhao1804 <124009006+xuanhao1804@users.noreply.github.com>
2024-10-16 00:10:21 +0700

    Develop.sprint1.hao2 (#19)
    * Define các core schema
    
    * update thêm adress, sửa tên folder routers>routes
    
    * update link bảng
    
    * fixname export of each router to return type middleware function
    
    * nothing
    
    * fix tạm thời cho intro của mỗi rạp
    
    * thêm import cho custom footer
    
    * fix nho
    
    ---------
    
    Co-authored-by: Joseph Nguyen <92445397+tattu2705@users.noreply.github.com>
    Co-authored-by: Lê Xuân Hào <144305850+haolx18@users.noreply.github.com>

--- back-end/routes/cinema.route.js
+++ back-end/routes/cinema.route.js
@@ -1,23 +1,11 @@
 
 const express = require('express');
 const cinemaController = require('../controllers/cinema.controller');
-const { cinema } = require('../models');
 const cinemaRouter = express.Router();
 
 cinemaRouter.get("/get-all",cinemaController.getAllCinema );
-<<<<<<< HEAD
-
-cinemaRouter.get("/get-by-city/:id",cinemaController.getCinemaByCity );
-
-module.exports = cinemaRouter;
-<<<<<<< HEAD
-=======
 
 cinemaRouter.get("/get-by-city/:id",cinemaController.getCinemaByCity );
 
 module.exports = cinemaRouter;
 
->>>>>>> dde8a87f72de5c55b187a16851633be527c7f1d6
-=======
-
->>>>>>> aefac208b30f52d76ab6b9fcec9e76a55004dba8
