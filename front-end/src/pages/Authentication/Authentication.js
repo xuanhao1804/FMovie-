341e28ec4bd22ab0a719d8134dfb174e8a2ebf86
tattu2705 <tattupro2705@gmail.com>
2024-10-19 16:12:20 +0700

    Develop login/signup function

--- front-end/src/pages/Authentication/Authentication.js
+++ front-end/src/pages/Authentication/Authentication.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import { Form, Input, Button, Tabs, DatePicker, Checkbox, message } from 'antd';
 import { UserOutlined, LockOutlined, MailOutlined, PhoneOutlined } from '@ant-design/icons';
-import { Carousel } from 'antd'
+import { useDispatch, useSelector } from 'react-redux';
+import { createUser, loginUser, saveUserData } from '../../reducers/UserReducer';
 import './Authentication.scss';
 
 const { TabPane } = Tabs;
@@ -9,37 +10,97 @@ const { TabPane } = Tabs;
 export default function Authentication() {
   const [form] = Form.useForm();
   const [activeTab, setActiveTab] = useState('login');
+  const [loading, setLoading] = useState(false);
+  const user = useSelector((state) => state.user);
+  if (user.user.account) {
+    window.location.href = '/';
+  }
 
-  const contentStyle = {
-    margin: 0,
-    height: '160px',
-    color: '#fff',
-    lineHeight: '160px',
-    textAlign: 'center',
-    background: '#364d79',
+  const dispatch = useDispatch();
+
+  const handleSignUp = async (values) => {
+    const data = {
+      fullname: values.name,
+      email: values.email,
+      phone: values.phone,
+      dob: values.dateOfBirth.$y + '-' + values.dateOfBirth.$M + '-' + values.dateOfBirth.$D,
+      password: values.password,
+      confirmPassword: values.confirmPassword,
+      agreement: values.agreement,
+      role: 'user'
+    }
+    setLoading(true);
+    try {
+      await dispatch(createUser(data));
+      message.success('User registered successfully!');
+      //reset form fields
+      form.resetFields();
+    } catch (error) {
+      message.error('Registration failed. Please try again.');
+    } finally {
+      setLoading(false);
+    }
   };
 
-  const onFinish = (values) => {
-    console.log('Form values:', values);
-    message.success('Form submitted successfully!');
+  const handleLogin = async (values) => {
+    const data = {
+      email: values.email_login,
+      password: values.password_login
+    }
+    try {
+      const rs = await dispatch(loginUser(data));
+      if (rs.payload.token) 
+      {
+        message.success('Login successful!');
+        const { password, ...userData } = rs.payload.account;
+        dispatch(saveUserData({ account: userData, token: rs.payload.token }));
+        window.location.href = '/';
+      }
+      else
+        message.error('Email or password is incorrect');
+    } catch (error) {
+      message.error('Login failed. Please try again.');
+    }
+  };
+
+  const handlePasswordRecovery = async (values) => {
+    setLoading(true);
+    // Implement actual password recovery logic here
+    console.log('Password recovery for:', values.email);
+    setLoading(false);
+    message.info('Password recovery instructions sent to your email.');
+  };
+
+  const validatePassword = (_, value) => {
+    const strongPassword = /^.{8,}$/;
+    if (!value) {
+      return Promise.reject('Please input your password!');
+    }
+    if (!strongPassword.test(value)) {
+      return Promise.reject('Password must be at least 8 characters long');
+    }
+    return Promise.resolve();
   };
 
   const renderLoginForm = () => (
-    <Form form={form} name="login" onFinish={onFinish} className="login-form">
+    <Form form={form} name="login" onFinish={handleLogin} className="login-form">
       <Form.Item
-        name="email"
-        rules={[{ required: true, message: 'Please input your Email!' }]}
+        name="email_login"
+        rules={[
+          { required: true, message: 'Please input your Email!' },
+          { type: 'email', message: 'Please enter a valid email!' }
+        ]}
       >
         <Input prefix={<MailOutlined />} placeholder="Email" />
       </Form.Item>
       <Form.Item
-        name="password"
-        rules={[{ required: true, message: 'Please input your Password!' }]}
+        name="password_login"
+        rules={[{ validator: validatePassword }]}
       >
         <Input.Password prefix={<LockOutlined />} placeholder="Password" />
       </Form.Item>
       <Form.Item>
-        <Button type="primary" htmlType="submit" className="login-form-button">
+        <Button type="primary" htmlType="submit" className="login-form-button" loading={loading}>
           Log in
         </Button>
         Or <a href="#" onClick={() => setActiveTab('recover')}>Forgot password</a>
@@ -48,7 +109,7 @@ export default function Authentication() {
   );
 
   const renderRegisterForm = () => (
-    <Form form={form} name="register" onFinish={onFinish} className="register-form">
+    <Form form={form} name="register" onFinish={handleSignUp} className="register-form">
       <Form.Item
         name="name"
         rules={[{ required: true, message: 'Please input your Name!' }]}
@@ -57,7 +118,10 @@ export default function Authentication() {
       </Form.Item>
       <Form.Item
         name="phone"
-        rules={[{ required: true, message: 'Please input your Phone Number!' }]}
+        rules={[
+          { required: true, message: 'Please input your Phone Number!' },
+          { pattern: /^\d{10}$/, message: 'Please enter a valid 10-digit phone number!' }
+        ]}
       >
         <Input prefix={<PhoneOutlined />} placeholder="Phone Number" />
       </Form.Item>
@@ -78,12 +142,26 @@ export default function Authentication() {
       </Form.Item>
       <Form.Item
         name="password"
-        rules={[
-          { required: true, message: 'Please input your Password!' },
-          { min: 6, message: 'Password must be at least 6 characters long!' }
-        ]}
+        rules={[{ validator: validatePassword }]}
       >
         <Input.Password prefix={<LockOutlined />} placeholder="Password" />
+      </Form.Item>
+      <Form.Item
+        name="confirmPassword"
+        dependencies={['password']}
+        rules={[
+          { required: true, message: 'Please confirm your password!' },
+          ({ getFieldValue }) => ({
+            validator(_, value) {
+              if (!value || getFieldValue('password') === value) {
+                return Promise.resolve();
+              }
+              return Promise.reject('The two passwords do not match!');
+            },
+          }),
+        ]}
+      >
+        <Input.Password prefix={<LockOutlined />} placeholder="Confirm Password" />
       </Form.Item>
       <Form.Item
         name="agreement"
@@ -95,7 +173,7 @@ export default function Authentication() {
         <Checkbox>I agree to the <a href="#">Terms of Use</a></Checkbox>
       </Form.Item>
       <Form.Item>
-        <Button type="primary" htmlType="submit" className="register-form-button">
+        <Button type="primary" htmlType="submit" className="register-form-button" loading={loading}>
           Register
         </Button>
       </Form.Item>
@@ -103,7 +181,7 @@ export default function Authentication() {
   );
 
   const renderRecoverForm = () => (
-    <Form form={form} name="recover" onFinish={onFinish} className="recover-form">
+    <Form form={form} name="recover" onFinish={handlePasswordRecovery} className="recover-form">
       <Form.Item
         name="email"
         rules={[
@@ -114,7 +192,7 @@ export default function Authentication() {
         <Input prefix={<MailOutlined />} placeholder="Email" />
       </Form.Item>
       <Form.Item>
-        <Button type="primary" htmlType="submit" className="recover-form-button">
+        <Button type="primary" htmlType="submit" className="recover-form-button" loading={loading}>
           Reset Password
         </Button>
       </Form.Item>
--- front-end/src/store.js
+++ front-end/src/store.js
@@ -1,10 +1,40 @@
-import { configureStore } from "@reduxjs/toolkit"
+import { configureStore, combineReducers } from "@reduxjs/toolkit"
 import UserReducer from "./reducers/UserReducer"
 import MovieReducer from "./reducers/MovieReducer"
+import storage from 'redux-persist/lib/storage'
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+  } from 'redux-persist'
+  
+  
+const rootReducer = combineReducers({
+    user: UserReducer,
+    movies: MovieReducer
+})
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['user']
+}
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 export const store = configureStore({
-    reducer: {
-        user: UserReducer,
-        movies: MovieReducer
-    }
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+        serializableCheck: {
+            ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+        }
+    }),
+
+    devTools: true
 })
+
+export const persistor = persistStore(store);
