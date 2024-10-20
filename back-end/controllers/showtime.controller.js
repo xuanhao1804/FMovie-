5260c8a59b1d75fd1d90932366b161f0a4810513
^Zin^ <144225954+Hminh11@users.noreply.github.com>
2024-10-20 08:49:58 +0700

    api-filter-showtime (#27)

--- back-end/controllers/showtime.controller.js
+++ back-end/controllers/showtime.controller.js
@@ -1,6 +1,33 @@
 const db = require("../models");
 
+const getShowtimebyDateandMoviesandCinema = async (req, res) => {
+    try {
+        const { cityId, movieId, date } = req.body;
 
 
-const ShowtimeController = {};
+        const cinemas = await db.cinema.find({ city: cityId })
+            .populate({
+                path: 'rooms',
+                populate: {
+                    path: 'showtimes',
+                    match: {
+                        movie: movieId,
+                        // Thêm so sánh ngày giờ ở đây
+                    }
+                }
+            })
+            .exec();
+        return res.status(200).json({ cinemas })
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            message: "Lỗi hệ thống Back-end"
+        });
+    }
+};
+
+
+
+const ShowtimeController = { getShowtimebyDateandMoviesandCinema };
 module.exports = ShowtimeController;
