4a75f0a833ad1fef0cb21fddbc460f246fd0a8ae
^Zin^ <144225954+Hminh11@users.noreply.github.com>
2024-10-20 13:58:48 +0700

    Backend booking (#28)
    * api-filter-showtime
    
    * fix-api-filter-showtime
    
    * fix-fe-homepage
    
    * update
    
    ---------
    
    Co-authored-by: Nguyen Duy Manh <140325027+dmanuhy@users.noreply.github.com>

--- back-end/server.js
+++ back-end/server.js
@@ -5,7 +5,7 @@ const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const db = require("./models");
 
-const { MovieRouter, CinemaRouter, RoomRouter, ShowtimeRouter, CityRouter, AccountRouter } = require('./routes');
+const { MovieRouter, CinemaRouter, RoomRouter, ShowtimeRouter, CityRouter, AccountRouter, BookingRouter } = require('./routes');
 
 const { createServer } = require("node:http");
 const { Server } = require("socket.io");
@@ -38,7 +38,7 @@ app.use("/room", RoomRouter);
 app.use("/showtime", ShowtimeRouter);
 app.use("/city", CityRouter);
 app.use("/user", AccountRouter);
-
+app.use("/booking", BookingRouter);
 io.on("connection", (socket) => {
     console.log("An user connect: ", socket.id);
 
