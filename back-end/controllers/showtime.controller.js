a8a110028e7481209a27456ac604994b65874f65
Hminh11 <minhvhhe170320@fpt.edu.vn>
2024-10-20 13:32:18 +0700

    fix-api-filter-showtime

--- back-end/controllers/showtime.controller.js
+++ back-end/controllers/showtime.controller.js
@@ -2,8 +2,27 @@ const db = require("../models");
 
 const getShowtimebyDateandMoviesandCinema = async (req, res) => {
     try {
-        const { cityId, movieId, date } = req.body;
+        let { cityId, movieId, date } = req.body;
 
+        // Nếu 'date' là null, sử dụng ngày hôm nay
+        if (!date) {
+            date = new Date();  // Lấy ngày hôm nay
+        } else {
+            date = new Date(date);  // Nếu có 'date' thì chuyển về dạng Date
+        }
+
+        // Đặt thời gian về 0:00 để chỉ lấy phần ngày
+        const startOfDay = new Date(date.setUTCHours(0, 0, 0, 0));
+        const endOfDay = new Date(startOfDay);
+        endOfDay.setDate(startOfDay.getDate() + 1); // Ngày kết thúc là đầu ngày hôm sau
+
+        // Lấy giờ hiện tại
+        const currentTime = new Date();
+        const currentHours = currentTime.getHours(); // Giờ hiện tại (theo múi giờ địa phương)
+        const currentMinutes = currentTime.getMinutes(); // Phút hiện tại (theo múi giờ địa phương)
+
+        // Chuyển đổi giờ hiện tại thành chuỗi HH:mm
+        const currentHourString = `${currentHours}:${currentMinutes < 10 ? '0' + currentMinutes : currentMinutes}`;
 
         const cinemas = await db.cinema.find({ city: cityId })
             .populate({
@@ -12,12 +31,24 @@ const getShowtimebyDateandMoviesandCinema = async (req, res) => {
                     path: 'showtimes',
                     match: {
                         movie: movieId,
-                        // Thêm so sánh ngày giờ ở đây
+                        'startAt.date': {
+                            $gte: startOfDay,  // Lấy từ đầu ngày
+                            $lt: endOfDay      // Đến trước ngày hôm sau
+                        },
                     }
                 }
             })
             .exec();
-        return res.status(200).json({ cinemas })
+        cinemas.forEach(cinema => {
+            cinema.rooms.forEach(room => {
+                room.showtimes.forEach(showtime => {
+                    // Lọc các times
+                    showtime.startAt.times = showtime.startAt.times.filter(time => time > currentHourString);
+                });
+            });
+        });
+
+        return res.status(200).json({ cinemas });
 
     } catch (error) {
         console.log(error);
@@ -26,6 +57,9 @@ const getShowtimebyDateandMoviesandCinema = async (req, res) => {
         });
     }
 };
+
+
+
 
 
 
