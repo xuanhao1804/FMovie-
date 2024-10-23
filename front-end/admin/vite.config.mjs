8dfade46c5c8041fef99ba0d338e7e137f564a6a
tattu2705 <tattupro2705@gmail.com>
2024-10-23 10:39:32 +0700

    Change folder structure

--- front-end/admin/vite.config.mjs
+++ front-end/admin/vite.config.mjs
@@ -45,7 +45,7 @@ export default defineConfig(() => {
       extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.scss'],
     },
     server: {
-      port: 3000,
+      port: 3001,
       proxy: {
         // https://vitejs.dev/config/server-options.html
       },
