d58d7b934cdad5fb46a085d94f539a2a154e2ec5
Hminh11 <minhvhhe170320@fpt.edu.vn>
2024-10-26 21:50:42 +0700

    Manage-PopCorn-Be

--- back-end/controllers/movie.controller.js
+++ back-end/controllers/movie.controller.js
@@ -50,29 +50,29 @@ const CreatnewMovie = async (req, res) => {
     try {
         const file = req.file;
         const { name, actors, director, duration, genres, video, studio, country, description, status, price } = req.body;
-        console.log(file.path)
-        // Tạo một đối tượng movie mới
-        const newMovie = new db.movie({
-            name,
-            director,
-            actors,
-            studio,
-            duration,
-            country,
-            genres,
-            image: file.path,
-            video,
-            description,
-            publicId: file.filename,
-            price,
-            status
-        });
-        const savedMovie = await newMovie.save();
-        res.status(201).json({
-            success: true,
-            data: savedMovie,
-            message: 'Movie created successfully!',
-        });
+        if (file?.path) {
+            const newMovie = new db.movie({
+                name,
+                director,
+                actors,
+                studio,
+                duration,
+                country,
+                genres,
+                image: file.path,
+                video,
+                description,
+                publicId: file.filename,
+                price,
+                status
+            });
+            const savedMovie = await newMovie.save();
+            res.status(201).json({
+                success: true,
+                data: savedMovie,
+                message: 'Movie created successfully!',
+            });
+        }
     } catch (error) {
         console.log(error);
         return res.status(500).json({
@@ -83,23 +83,44 @@ const CreatnewMovie = async (req, res) => {
 const Editmovie = async (req, res) => {
     try {
         const { id } = req.params;
+        const file = req.file;
+        console.log(id)
         const { name, director, actors, studio, price, duration, country, genres, limit, video, description, status } = req.body;
-        const updatedMovie = await db.movie.findByIdAndUpdate(id, {
-            name,
-            director,
-            actors,
-            studio,
-            price,
-            duration,
-            country,
-            genres,
-            limit,
-            video,
-            description,
-            status
-        }, { new: true }); // Option to return the updated document
-
-        return res.status(200).json(updatedMovie); // Return the updated movie
+        if (file) {
+            const updatedMovie = await db.movie.findByIdAndUpdate(id, {
+                name,
+                director,
+                actors,
+                studio,
+                price,
+                duration,
+                country,
+                genres,
+                limit,
+                image: file.path,
+                video,
+                description,
+                status
+            }, { new: true });
+            return res.status(200).json(updatedMovie);
+        }
+        if (!file) {
+            const updatedMovie = await db.movie.findByIdAndUpdate(id, {
+                name,
+                director,
+                actors,
+                studio,
+                price,
+                duration,
+                country,
+                genres,
+                limit,
+                video,
+                description,
+                status
+            }, { new: true });
+            return res.status(200).json(updatedMovie);
+        }
     } catch (error) {
         console.log(error);
         return res.status(500).json({
