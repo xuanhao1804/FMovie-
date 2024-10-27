4761ed2235559ee1198243f065080525f9be253f
^Zin^ <144225954+Hminh11@users.noreply.github.com>
2024-10-27 12:11:52 +0700

    Backend booking rs1 (#42)
    * Add footer
    
    * api-filter-showtime
    
    * fix-api-filter-showtime
    
    * fix-fe-homepage
    
    * update
    
    * fixing
    
    * api-payment
    
    * fix-model
    
    * fix-api-showtime
    
    * Manage-movie
    
    * Fix admin
    
    * Fix-manage-movie
    
    * Update Code
    
    * Update
    
    * Update
    
    * Fixing
    
    * Booking-flow
    
    * Update Booking.js
    
    * Manage-PopCorn-Be
    
    ---------
    
    Co-authored-by: tattu2705 <tattupro2705@gmail.com>
    Co-authored-by: Nguyen Duy Manh <140325027+dmanuhy@users.noreply.github.com>

--- back-end/controllers/popcorn.controller.js
+++ back-end/controllers/popcorn.controller.js
@@ -21,8 +21,74 @@ const getAllPopcorn = async (req, res) => {
         });
     }
 };
+const CreateNewPopCorn = async (req, res) => {
+    const file = req.file;
+    const { name, description, price } = req.body;
+    try {
+        const newPopcorn = new db.popcorn({
+            name,
+            description,
+            price,
+            image: file.path
+        });
+        const savePopcorn = await newPopcorn.save();
+        return res.status(201).json(savePopcorn);
+    } catch (error) {
+        console.error('Error creating new cinema:', error);
+        return res.status(500).json({ message: 'Error creating new cinema', error });
+    }
+};
+const EditPopCorn = async (req, res) => {
+    const file = req.file;
+    console.log(req.file);
+    const { id } = req.body;
+    const { name, description, price } = req.body;
+    try {
+        if (file?.path) {
+            const updatedPopcorn = await db.popcorn.findByIdAndUpdate(
+                id,
+                {
+                    name,
+                    image: file.path,
+                    description,
+                    price,
+                },
+                { new: true }
+            );
+            return res.status(200).json(updatedPopcorn);
+        }
+        if (!file) {
+            const updatedPopcorn = await db.popcorn.findByIdAndUpdate(
+                id,
+                {
+                    name,
+                    description,
+                    price,
+                },
+                { new: true }
+            );
+            return res.status(200).json(updatedPopcorn);
+        }
+    } catch (error) {
+        console.error('Error creating new cinema:', error);
+        return res.status(500).json({ message: 'Error creating new cinema', error });
+    }
+};
+const DeletePopCorn = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const deletedMovie = await db.popcorn.findByIdAndDelete(id);
+        if (!deletedMovie) {
+            return res.status(404).json({ message: "PopCorn không tồn tại" });
+        }
+        return res.status(200).json({ message: "PopCorn đã được xóa thành công" });
+    } catch (error) {
+        console.error('Error creating new cinema:', error);
+        return res.status(500).json({ message: 'Error creating new cinema', error });
+    }
+};
 
 const PopcornController = {
-    getAllPopcorn
+    getAllPopcorn, CreateNewPopCorn, EditPopCorn, DeletePopCorn
 };
 module.exports = PopcornController;
--- back-end/routes/movie.route.js
+++ back-end/routes/movie.route.js
@@ -9,7 +9,7 @@ movieRouter.use(bodyParser.json());
 
 movieRouter.get("/get-all", MovieController.getAllMovie);
 movieRouter.post("/create", uploadCloud.single('image'), MovieController.CreatnewMovie);
-movieRouter.put("/update/:id", MovieController.Editmovie);
+movieRouter.put("/update/:id", uploadCloud.single('image'), MovieController.Editmovie);
 movieRouter.get("/detail/:id", MovieController.getMovieByID)
 movieRouter.delete("/delete/:id", MovieController.Deletemovie)
 
