35cd59fa59fc3f82ab6e5e83964c601ae3bc25bf
tattu2705 <tattupro2705@gmail.com>
2024-11-06 21:52:39 +0700

    Fix small bbug

--- back-end/routes/account.route.js
+++ back-end/routes/account.route.js
@@ -8,6 +8,7 @@ accountRoute.post("/sign-in", validateSignIn, accountController.signIn);
 accountRoute.post("/send-mail", accountController.sendMail);
 accountRoute.post("/verify-otp", accountController.verifyOTP);
 accountRoute.post("/reset-password", accountController.resetPassword);
+accountRoute.get('/total-user', accountController.getTotalUser);
 accountRoute.get('/:id', accountController.getAccount);
 accountRoute.put('/:id', accountController.updateAccount);
 accountRoute.get('/', accountController.getAllAccounts);
