506bdc39e099b6a8a956cf26e4efca218b4b4b59
tattu2705 <tattupro2705@gmail.com>
2024-11-07 14:12:04 +0700

    Dev update user info

--- front-end/src/pages/UserProfile/UserProfile.js
+++ front-end/src/pages/UserProfile/UserProfile.js
@@ -1,17 +1,22 @@
 import { useEffect, useState } from "react"
 import "./UserProfile.scss"
-import { useSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { Link, useNavigate } from "react-router-dom";
-import { Col, Row } from "antd";
 import { BookingService } from "../../services/BookingService";
 import { getVietnameseDate } from "../../utils/dateUtils";
 import { NumericFormat } from "react-number-format";
+import { message } from "antd";
 import { bookingStatus } from "../../utils/bookingUtils";
+import { Button } from "antd";
+import { resetInfo } from "../../reducers/UserReducer";
 
 const UserProfile = () => {
 
     const { user } = useSelector((state) => state.user);
+    const [userData, setUserData] = useState(user.account)
     const [userHistory, setUserHistory] = useState([])
+    const [errors, setErrors] = useState({});
+    const dispatch = useDispatch()
     const navigate = useNavigate()
 
     const fetchUserHistory = async () => {
@@ -23,6 +28,85 @@ const UserProfile = () => {
         }
     }
 
+    const validateField = (field, value) => {
+        let error = null;
+        if (!value) {
+            error = 'Trường này là bắt buộc.';
+        } else {
+            switch (field) {
+                case 'fullname':
+                    error = value.length < 2 ? 'Tên phải có ít nhất 2 ký tự.' : '';
+                    break;
+                case 'email':
+                    error = !/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/.test(value) ? 'Email không hợp lệ.' : '';
+                    break;
+                case 'phone':
+                    error = !/^\d{10}$/.test(value) ? 'Số điện thoại phải đủ 10 chữ số.' : '';
+                    break;
+                default:
+                    break;
+            }
+        }
+        return error;
+    };
+
+    const handleUpdate = async () => {
+        if (errors && Object.keys(errors).length > 0) {
+            message.error('Vui lòng kiểm tra lại thông tin.')
+            return
+        }
+        const { _id, email, phone, fullname, dob } = userData
+        const data = {
+            _id,
+            email,
+            phone,
+            fullname,
+            dob
+        }
+        fetch('http://localhost:9999/account/update-account', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        }).then(async (response) => {
+            const result = await response.json();
+            if (result.success) {
+                message.success('Cập nhật thông tin thành công!')
+                dispatch(resetInfo(result.data))
+            } else {
+                message.error('Cập nhật thông tin thất bại!')
+            }
+        }).catch((error) => {
+            console.error('Error:', error);
+        }
+        );
+    }
+
+    const handleDataChange = (e) => {
+        const { id, value } = e.target;
+
+        setUserData((prevData) => ({
+            ...prevData,
+            [id]: value,
+        }));
+        const error = validateField(id, value);
+
+        setErrors((prevErrors) => {
+            const newErrors = { ...prevErrors };
+
+            if (error) {
+                // Nếu có lỗi, cập nhật lỗi vào đối tượng errors
+                newErrors[id] = error;
+            } else {
+                // Nếu không có lỗi, xóa lỗi khỏi đối tượng errors
+                delete newErrors[id];
+            }
+
+            return newErrors;
+        });
+    };
+
     useEffect(() => {
         if (user) {
             fetchUserHistory()
@@ -33,30 +117,58 @@ const UserProfile = () => {
 
     return (
         <div className="user-profile content-width-padding content-height-padding d-flex gap-4">
-            {console.log(userHistory)}
             <div className="user-profile-info w-25 p-3">
                 <div className="fs-5 pb-2 text-center border-bottom mb-3">
                     Thông tin người dùng
                 </div>
                 <div className="mb-3">
-                    <label for="username" className="form-label">Họ và tên</label>
-                    <input disabled value={user?.account?.fullname} type="text" className="form-control" id="username" />
+                    <label htmlFor="fullname" className="form-label">Họ và tên</label>
+                    <input
+                        value={userData?.fullname || ''}
+                        type="text"
+                        onChange={handleDataChange}
+                        className="form-control"
+                        id="fullname"
+                    />
+                    {errors.fullname && <span className="text-danger">{errors.fullname}</span>}
                 </div>
                 <div className="mb-3">
-                    <label for="Email" className="form-label">Email</label>
-                    <input disabled value={user?.account?.email} type="email" className="form-control" id="Email" />
+                    <label htmlFor="email" className="form-label">Email</label>
+                    <input
+                        value={userData?.email || ''}
+                        type="email"
+                        onChange={handleDataChange}
+                        className="form-control"
+                        id="email"
+                    />
+                    {errors.email && <span className="text-danger">{errors.email}</span>}
                 </div>
                 <div className="mb-3">
-                    <label for="phone" className="form-label">Số điện thoại</label>
-                    <input disabled value={user?.account?.phone} type="text" className="form-control" id="phone" />
+                    <label htmlFor="phone" className="form-label">Số điện thoại</label>
+                    <input
+                        value={userData?.phone || ''}
+                        type="text"
+                        onChange={handleDataChange}
+                        className="form-control"
+                        id="phone"
+                    />
+                    {errors.phone && <span className="text-danger">{errors.phone}</span>}
                 </div>
                 <div className="mb-3">
-                    <label for="dob" className="form-label">Ngày sinh</label>
-                    <input disabled value={user?.account?.dob.split('T')[0]} type="date" className="form-control" id="dob" />
+                    <label htmlFor="dob" className="form-label">Ngày sinh</label>
+                    <input
+                        value={userData?.dob ? userData.dob.split('T')[0] : ''}
+                        onChange={handleDataChange}
+                        type="date"
+                        className="form-control"
+                        id="dob"
+                    />
+                    {errors.dob && <span className="text-danger">{errors.dob}</span>}
                 </div>
                 <div className="mb-3">
-                    <label for="password" className="form-label">Mật khẩu</label>
-                    <input disabled value={"HARDCODE Rồi"} type="password" className="form-control" id="password" />
+                    <Button onClick={handleUpdate} type="primary" htmlType="submit" className="update-btn">
+                        Cập Nhật
+                    </Button>
                 </div>
             </div>
             <div className="user-profile-history w-75 p-3">
